1039cabe42f7d21b8808723b5602120cad14cb1b
Funky Noodles <nickasds@hotmail.com>
2016-06-08 22:39:25 +0800

    sample

---
+++ FunkyBudget/src/io/github/funkynoodles/FXMLTableViewController.java
@@ -0,0 +1,11 @@
+package io.github.funkynoodles;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class FXMLTableViewController {
+
+    @FXML
+    protected void addPerson(ActionEvent event) {
+    }
+}
